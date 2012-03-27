fe762648bb16b0c2f989c915a7f63032453637e5
Kyle Grovers <kyle.grovers@gmail.com>
2012-03-28 08:48:59 +1030

    Starting the mummakitchen

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index
+    @title = "Home"
+  end
+
+  def about
+    @title = "About Us"
+  end
+
+  def contact
+    @title = "Contact"
+  end
+  
+  def menu
+    @title = "Menu"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mummaskitchen::Application.config.secret_token = '107c330b137ca74b1f87113c347361777737907b6d5985a195e8d881f1b26a1caefa5a47fa89605bed2b40188585e3e579b74971ac3ce706385408dfd4265be2'
