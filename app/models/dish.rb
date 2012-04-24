f1f9540fb76bd17cc79cbc099c9b4c3d98f3f611
Kyle Grovers <kyle.grovers@gmail.com>
2012-04-24 14:01:32 +0930

    changed the banner and fixed the pagination

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -10,35 +10,43 @@ class Dish < ActiveRecord::Base
   
   def self.alldishes(menu_id=nil, page)
         if !menu_id.nil?
-          paginate  :all, :per_page=>8, :page=>page,
+          dishes = find :all,
                     :select=>"DISTINCT \"dishes\".*",  
                     :conditions=>"menu_id=#{menu_id}"
         else
-          paginate  :all, :per_page=>8, :page=>page,
+          dishes = find :all,
                     :select=>"DISTINCT \"dishes\".*"          
         end
+        
+       dishes.paginate  :per_page=>8, :page=>page 
   end
   
   # use this method to find specific dishes on the menu
   # this populates the dishes page after the menu is selected
   def self.freezer(menu_id, freezer, page)
-    paginate :all, :per_page=>5, :page=>page,
-          :select=>"DISTINCT \"dishes\".*", 
-          :joins=>:prices, 
-          :conditions=>"menu_id=#{menu_id} and freezer='#{freezer}'"
+    dishes = find :all, :select=>"DISTINCT \"dishes\".*", 
+                  :joins=>:prices, 
+                  :conditions=>"menu_id=#{menu_id} and freezer='#{freezer}'"
+    
+    dishes.paginate :per_page=>5, :page=>page
+
   end
   
   # use this method to access all the dishes in the freezer
   def self.freezerall(page)
-    paginate :all, :per_page=>5, :page=>page,
-          :select=>"DISTINCT \"dishes\".*", 
-          :joins=>:prices,
-          :conditions=>"freezer='t'",
-          :order=>"menu_id, dish_id"
+    dishes = find :all, :select=>"DISTINCT \"dishes\".*", 
+                  :joins=>:prices,
+                  :conditions=>"freezer='t'",
+                  :order=>"menu_id, dish_id"
+    
+    dishes.paginate :per_page=>5, :page=>page
+          
   end
   
   def images(dish_id)
     Photos.find_all_by_dish_id(dish_id)
   end
+  
+  
   
 end
